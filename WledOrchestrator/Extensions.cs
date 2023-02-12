02990bdebb49ceb4c21f505c5e684100705b0de7
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-02-12 03:16:03 +0100

    Add Lerp function for colors

--- WledOrchestrator/Extensions.cs
+++ WledOrchestrator/Extensions.cs
@@ -57,6 +57,10 @@ namespace WledOrchestrator
         }
 
         public static string ToHex(this Color c) => $"{c.R:X2}{c.G:X2}{c.B:X2}";
+        public static Color Lerp(this Color c, Color b, double x) =>
+            Color.FromArgb((int)(c.R * (1 - x) + b.R * x),
+                           (int)(c.G * (1 - x) + b.G * x),
+                           (int)(c.B * (1 - x) + b.B * x));
 
         public static async Task<string> GetHttpResponseFrom(this string url, int timeout = 5)
         {
