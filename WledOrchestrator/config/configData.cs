d87ce6161626806a93a76f286a9d53bc3cb45f6f
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-02-19 17:56:17 +0100

    Add config classes

---
+++ WledOrchestrator/config/configData.cs
@@ -0,0 +1,25 @@
+﻿using Notes.Comms;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Configuration
+{
+    public class ConfigData
+    {
+        public DateTime saveTime;
+
+        public Point Pos;
+        public Size Size;
+        public Color BackColor;
+
+        public List<Note> Notes;
+
+        public ConfigData()
+        {
+            Pos = new Point(-1, -1);
+            Size = new Size(-1, -1);
+            Notes = new List<Note>();
+        }
+    }
+}
