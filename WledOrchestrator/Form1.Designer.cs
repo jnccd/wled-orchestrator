8589dd048b3b5ccf858e89149fd40a35d0627da6
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-04-11 04:39:09 +0200

    Stub out new GUI

--- WledOrchestrator/Form1.Designer.cs
+++ WledOrchestrator/Form1.Designer.cs
@@ -34,7 +34,16 @@
             this.ledButtonTemplate = new System.Windows.Forms.Button();
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.stateLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.profileButtonTemplate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.ledsPanel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // ledsPanel
@@ -43,16 +52,16 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ledsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ledsPanel.Controls.Add(this.ledButtonTemplate);
-            this.ledsPanel.Location = new System.Drawing.Point(12, 12);
+            this.ledsPanel.Location = new System.Drawing.Point(0, 3);
             this.ledsPanel.Name = "ledsPanel";
-            this.ledsPanel.Size = new System.Drawing.Size(776, 57);
+            this.ledsPanel.Size = new System.Drawing.Size(791, 39);
             this.ledsPanel.TabIndex = 0;
             // 
             // ledButtonTemplate
             // 
             this.ledButtonTemplate.Location = new System.Drawing.Point(3, 3);
             this.ledButtonTemplate.Name = "ledButtonTemplate";
-            this.ledButtonTemplate.Size = new System.Drawing.Size(67, 49);
+            this.ledButtonTemplate.Size = new System.Drawing.Size(67, 30);
             this.ledButtonTemplate.TabIndex = 1;
             this.ledButtonTemplate.Text = "button1";
             this.ledButtonTemplate.UseVisualStyleBackColor = true;
@@ -69,19 +78,67 @@
             // 
             this.stateLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.stateLabel.AutoSize = true;
-            this.stateLabel.Location = new System.Drawing.Point(12, 426);
+            this.stateLabel.Location = new System.Drawing.Point(12, 459);
             this.stateLabel.Name = "stateLabel";
             this.stateLabel.Size = new System.Drawing.Size(42, 15);
             this.stateLabel.TabIndex = 1;
             this.stateLabel.Text = "Status:";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Controls.Add(this.profileButtonTemplate);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 69);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(791, 39);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // profileButtonTemplate
+            // 
+            this.profileButtonTemplate.Location = new System.Drawing.Point(3, 3);
+            this.profileButtonTemplate.Name = "profileButtonTemplate";
+            this.profileButtonTemplate.Size = new System.Drawing.Size(67, 30);
+            this.profileButtonTemplate.TabIndex = 1;
+            this.profileButtonTemplate.Text = "button1";
+            this.profileButtonTemplate.UseVisualStyleBackColor = true;
+            this.profileButtonTemplate.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ledsPanel);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 111);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 129);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(791, 327);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(637, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 319);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(815, 483);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.stateLabel);
-            this.Controls.Add(this.ledsPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "WLED Orchestrator";
@@ -89,6 +146,10 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ledsPanel.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,5 +161,10 @@
         private Button ledButtonTemplate;
         private NotifyIcon notifyIcon;
         private Label stateLabel;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button profileButtonTemplate;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
     }
 }
