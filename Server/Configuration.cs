e16c5a8b5fc06be6acb936a325213f85ee4aa723
jnccd <kobidogao@outlook.com>
2024-10-27 20:20:03 +0100

    Register services using a custom attribute

--- Server/Configuration.cs
+++ Server/Configuration.cs
@@ -1,18 +1,43 @@
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using Server.WledCommunicator;
+using Server.Services.WledCommunicator;
 
 namespace Server
 {
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class AutoRegister(ServiceRegisterType serviceRegisterType, Type serviceType) : Attribute
+    {
+        public readonly ServiceRegisterType serviceRegisterType = serviceRegisterType;
+        public readonly Type serviceType = serviceType;
+    }
+
+    public enum ServiceRegisterType { Singleton, Transient }
+
     public static class Configuration
     {
         public static void RegisterServices(this WebApplicationBuilder builder)
         {
-            //builder.Configuration.GetSection(nameof(BasicAuthOptions)).Bind(new BasicAuthOptions());
+            Type[] assemblyTypes = (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                                    from assemblyType in domainAssembly.GetTypes()
+                                    where assemblyType.Module == typeof(Configuration).Module
+                                    select assemblyType).ToArray();
+            var serviceTypes = (from interfaceType in assemblyTypes
+                                where interfaceType.CustomAttributes.Any(x => x.AttributeType == typeof(AutoRegister))
+                                    && interfaceType.IsInterface
+                                select interfaceType).ToList();
 
-            builder.Services
-                .AddSingleton<IWledCommunicatorService, WledCommunicatorService>()
-                ;
+            foreach (var interfaceType in serviceTypes)
+            {
+                var attr = interfaceType.CustomAttributes.First(x => x.AttributeType == typeof(AutoRegister));
+
+                var args = attr.ConstructorArguments;
+                ServiceRegisterType serviceRegisterType = (ServiceRegisterType)(args[0].Value ?? 0);
+                Type? serviceImplType = (Type?)args[1].Value;
+                if (serviceImplType == null) continue;
+
+                if (serviceRegisterType == ServiceRegisterType.Singleton)
+                    builder.Services.AddSingleton(interfaceType, serviceImplType);
+                else if (serviceRegisterType == ServiceRegisterType.Transient)
+                    builder.Services.AddTransient(interfaceType, serviceImplType);
+            }
         }
     }
 }
---
+++ Server/Services/UpdaterService.cs
@@ -0,0 +1,12 @@
+namespace Server.Services;
+
+[AutoRegister(ServiceRegisterType.Singleton, typeof(UpdaterService))]
+public interface IUpdaterService
+{
+
+}
+
+public class UpdaterService
+{
+
+}
