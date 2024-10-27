c61ed4f8284b40f53cb39837fd60840febcc984f
Niklas Carstensen <kobidogao@yahoo.com>
2024-10-27 22:30:32 +0100

    Rename RegisterImplementation attribute

--- Server/Configuration.cs
+++ Server/Configuration.cs
@@ -3,7 +3,7 @@ using Server.Services.WledCommunicator;
 namespace Server
 {
     [AttributeUsage(AttributeTargets.Interface)]
-    public class AutoRegister(ServiceRegisterType serviceRegisterType, Type serviceType) : Attribute
+    public class RegisterImplementation(ServiceRegisterType serviceRegisterType, Type serviceType) : Attribute
     {
         public readonly ServiceRegisterType serviceRegisterType = serviceRegisterType;
         public readonly Type serviceType = serviceType;
@@ -20,13 +20,13 @@ namespace Server
                                     where assemblyType.Module == typeof(Configuration).Module
                                     select assemblyType).ToArray();
             var serviceTypes = (from interfaceType in assemblyTypes
-                                where interfaceType.CustomAttributes.Any(x => x.AttributeType == typeof(AutoRegister))
+                                where interfaceType.CustomAttributes.Any(x => x.AttributeType == typeof(RegisterImplementation))
                                     && interfaceType.IsInterface
                                 select interfaceType).ToList();
 
             foreach (var interfaceType in serviceTypes)
             {
-                var attr = interfaceType.CustomAttributes.First(x => x.AttributeType == typeof(AutoRegister));
+                var attr = interfaceType.CustomAttributes.First(x => x.AttributeType == typeof(RegisterImplementation));
 
                 var args = attr.ConstructorArguments;
                 ServiceRegisterType serviceRegisterType = (ServiceRegisterType)(args[0].Value ?? 0);
