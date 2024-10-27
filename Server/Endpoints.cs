252b031b1f5a7515c93940ee82bb14868be16e5b
Niklas Carstensen <kobidogao@yahoo.com>
2024-10-27 11:34:58 +0100

    Init server

---
+++ Server/Endpoints.cs
@@ -0,0 +1,26 @@
+using Server.Helper;
+
+namespace Server
+{
+    public static class NotesEndpoints
+    {
+        public static void RegisterWledOrchestratorEndpoints(this IEndpointRouteBuilder routes, IServiceProvider services)
+        {
+            routes.MapGet("/hewwo", () =>
+            {
+                return Results.Extensions.Html(@$"<!doctype html>
+            <html>
+                <head>
+                    <title>Hewwo</title>
+                    <style>
+                        body {{font-family: sans-serif;}}
+                    </style>
+                </head>
+                <body>
+                    <h1>Hewwo Wowld :3</h1>
+                </body>
+            </html>");
+            });
+        }
+    }
+}
