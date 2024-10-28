0000d7f130c9734d340a4e95eda58da4b02f4aa5
jnccd <kobidogao@outlook.com>
2024-10-28 09:58:52 +0100

    Refactor

--- Server/Endpoints.cs
+++ Server/Endpoints.cs
@@ -4,22 +4,22 @@ namespace Server
 {
     public static class WledOrchestratorEndpoints
     {
-        public static void RegisterWledOrchestratorEndpoints(this IEndpointRouteBuilder routes, IServiceProvider services)
+        public static void RegisterEndpoints(this IEndpointRouteBuilder routes, IServiceProvider services)
         {
             routes.MapGet("/hewwo", () =>
             {
                 return Results.Extensions.Html(@$"<!doctype html>
-            <html>
-                <head>
-                    <title>Hewwo</title>
-                    <style>
-                        body {{font-family: sans-serif;}}
-                    </style>
-                </head>
-                <body>
-                    <h1>Hewwo Wowld :3</h1>
-                </body>
-            </html>");
+                    <html>
+                        <head>
+                            <title>Hewwo</title>
+                            <style>
+                                body {{font-family: sans-serif;}}
+                            </style>
+                        </head>
+                        <body>
+                            <h1>Hewwo Wowld :3</h1>
+                        </body>
+                    </html>");
             });
         }
     }
