e9beff526c1b24d57e19071471f00a4d87382be3
Niklas Carstensen <kobidogao@yahoo.com>
2024-10-28 18:15:14 +0100

    Refactor

--- Server/Endpoints.cs
+++ Server/Endpoints.cs
@@ -19,17 +19,17 @@ public static class WledOrchestratorEndpoints
         webApp.MapGet("/hewwo", () =>
         {
             return Results.Extensions.Html(@$"<!doctype html>
-                    <html>
-                        <head>
-                            <title>Hewwo</title>
-                            <style>
-                                body {{font-family: sans-serif;}}
-                            </style>
-                        </head>
-                        <body>
-                            <h1>Hewwo Wowld :3</h1>
-                        </body>
-                    </html>");
+                <html>
+                    <head>
+                        <title>Hewwo</title>
+                        <style>
+                            body {{font-family: sans-serif;}}
+                        </style>
+                    </head>
+                    <body>
+                        <h1>Hewwo Wowld :3</h1>
+                    </body>
+                </html>");
         });
     }
 }
