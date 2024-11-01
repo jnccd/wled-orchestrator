a023c8c6e66c3e500f2535b5b1bc0f5f1b792d50
jnccd <kobidogao@outlook.com>
2024-11-01 15:42:36 +0100

    Remove VITE_DEV_BACKEND_ADDRESS

--- Frontend/src/services/api-client.ts
+++ Frontend/src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: import.meta.env.VITE_DEV_BACKEND_ADDRESS ? import.meta.env.VITE_DEV_BACKEND_ADDRESS : window.location.href,
+    baseURL: window.location.href,
     withCredentials: false,
 })
