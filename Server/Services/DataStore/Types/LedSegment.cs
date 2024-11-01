f6aa11f6b353821d695b2ff6a7c9f5f77de25aa7
Niklas Carstensen <kobidogao@yahoo.com>
2024-11-01 23:34:32 +0100

    Add todo

--- Server/Services/DataStore/Types/LedSegment.cs
+++ Server/Services/DataStore/Types/LedSegment.cs
@@ -1,3 +1,3 @@
 namespace Server.Services.DataStore.Types;
 
-public record LedSegment(string WledServerAddress, int SegmentIndex);
+public record LedSegment(string WledServerAddress, int SegmentIndex); // TODO: Connected bool
