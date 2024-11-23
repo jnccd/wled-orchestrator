184ef476c69dccc98f8dfa3a8ca5f1ebe75798ce
jnccd <kobidogao@outlook.com>
2024-11-23 22:12:17 +0100

    Set dragging area

--- Frontend/src/components/DraggableButton.tsx
+++ Frontend/src/components/DraggableButton.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
+const debuggingLogs = false;
+
 interface Props {
   ownId: string;
   buttonName: string;
@@ -10,14 +12,15 @@ const DraggableButton = ({ ownId, buttonName }: Props) => {
   const [dragging, setDragging] = useState(false);
   const [draggingStartPos, setDraggingStartPos] = useState([0, 0]);
   const dragY = false;
+  const dragAreaPaddingX = 200;
 
   const dragMouseDown = (e: React.MouseEvent) => {
-    console.log("dragMouseDown");
+    if (debuggingLogs) console.log("dragMouseDown");
     e.preventDefault();
 
     var thisInDocument = document.getElementById(ownId);
     if (thisInDocument === null) {
-      console.log("dragMouseMove, cant find thisInDocument");
+      if (debuggingLogs) console.log("dragMouseMove, cant find thisInDocument");
       return;
     }
 
@@ -25,13 +28,14 @@ const DraggableButton = ({ ownId, buttonName }: Props) => {
       e.clientX - (parseInt(thisInDocument.style.left) || 0),
       e.clientY - (parseInt(thisInDocument.style.top) || 0),
     ]);
+    thisInDocument.style.transition = "";
     thisInDocument.style.zIndex = "10";
-    console.log(draggingStartPos);
+    if (debuggingLogs) console.log(draggingStartPos);
     setDragging(true);
   };
 
   const dragMouseMove = (e: React.MouseEvent) => {
-    console.log("dragMouseMove, " + dragging + ownId);
+    if (debuggingLogs) console.log("dragMouseMove, " + dragging + ownId);
     if (!dragging) {
       return;
     }
@@ -40,40 +44,66 @@ const DraggableButton = ({ ownId, buttonName }: Props) => {
 
     var thisInDocument = document.getElementById(ownId);
     if (thisInDocument === null) {
-      console.log("dragMouseMove, cant find thisInDocument");
+      if (debuggingLogs) console.log("dragMouseMove, cant find thisInDocument");
       return;
     }
 
-    thisInDocument.style.left = e.clientX - draggingStartPos[0] + "px";
-    if (dragY) {
-      thisInDocument.style.top = e.clientY - draggingStartPos[1] + "px";
-    }
+    setPos(
+      thisInDocument,
+      e.clientX - draggingStartPos[0],
+      e.clientY - draggingStartPos[1]
+    );
   };
 
   const dragMouseUp = () => {
-    console.log("dragMouseUp");
+    if (debuggingLogs) console.log("dragMouseUp");
     var thisInDocument = document.getElementById(ownId);
     if (thisInDocument === null) {
-      console.log("dragMouseMove, cant find thisInDocument");
+      if (debuggingLogs) console.log("dragMouseMove, cant find thisInDocument");
       return;
     }
 
+    thisInDocument.style.transition = "left 0.4s";
     thisInDocument.style.zIndex = "1";
+    setPos(thisInDocument, 0, 0);
     setDragging(false);
   };
+
+  const setPos = (e: HTMLElement, x: number, y: number) => {
+    e.style.left = x + "px";
+    if (dragY) {
+      e.style.top = y + "px";
+    }
+
+    const boundingRect = e.getBoundingClientRect();
+    if (boundingRect.left < dragAreaPaddingX) {
+      if (debuggingLogs) console.log("too far left!");
+      const left = parseInt(e.style.left.split("p")[0]);
+      e.style.left = String(left + (dragAreaPaddingX - boundingRect.x)) + "px";
+    }
+    if (boundingRect.right > document.body.clientWidth - dragAreaPaddingX) {
+      if (debuggingLogs) console.log("too far right!");
+      const left = parseInt(e.style.left.split("p")[0]);
+      e.style.left =
+        String(
+          left +
+            (document.body.clientWidth - dragAreaPaddingX - boundingRect.right)
+        ) + "px";
+    }
+  };
+
+  document.onmouseup = dragMouseUp;
 
   return (
     <Button
       id={ownId}
       position={"relative"}
       className="wledServerButton"
-      //transition="all 0.5s"
       margin={2}
       size="lg"
       cursor={"move"}
       onMouseDown={dragMouseDown}
       onMouseMove={dragMouseMove}
-      onMouseUp={dragMouseUp}
     >
       {buttonName}
     </Button>
