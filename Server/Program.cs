3660585563d83fa9551e348812bf19f506c49317
Niklas Carstensen <kobidogao@yahoo.com>
2024-11-24 12:33:32 +0100

    Add swagger

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,7 @@ builder.ConfigureWebhost();
 builder.RegisterServices();
 
 var app = builder.Build();
+app.EnableSwagger();
 app.RegisterEndpoints();
 app.Services.StartBackgroundServices();
 app.Run();
