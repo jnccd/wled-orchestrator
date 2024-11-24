7a2e93629a556834d2a8f6d71aac7a043a27622f
Niklas Carstensen <kobidogao@yahoo.com>
2024-11-24 17:13:28 +0100

    Add possible custom names to ledSegments

--- Server/Services/DataStore/Types/LedSegment.cs
+++ Server/Services/DataStore/Types/LedSegment.cs
@@ -1,3 +1,3 @@
 namespace Server.Services.DataStore.Types;
 
-public record LedSegment(string WledServerAddress, int SegmentIndex); // TODO: Connected bool
+public record LedSegment(string WledServerAddress, int SegmentIndex, string? Name = null);
