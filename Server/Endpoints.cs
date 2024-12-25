d44dda9a51b38067a833c45824c6c705515b3a4a
jnccd <kobidogao@outlook.com>
2024-12-25 04:48:03 +0100

    Add state editing endpoints

--- Server/Endpoints.cs
+++ Server/Endpoints.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using Server.Helper;
 using Server.Services.DataStore;
+using Server.Services.DataStore.Types;
 using Server.Services.WledCommunicator;
 namespace Server;
 
@@ -55,15 +57,52 @@ public static class WledOrchestratorEndpoints
             [FromServices] DataStoreService dataStore,
             [FromBody, Required] DataStoreRoot newState) =>
         {
-            try
+            dataStore.LoadFrom(JsonSerializer.Serialize(newState));
+            dataStore.Save();
+        });
+
+        app.MapPost("/state/createGroup", (
+            [FromServices] DataStoreService dataStore,
+            string? idOfFirstGroupSegment) =>
+        {
+            (var segmentGroup, var segment) = dataStore.Data.Groups.Select(g => (g, g.LedSegments.Where(x => x.ReadonlyId == idOfFirstGroupSegment).FirstOrDefault())).FirstOrDefault();
+            if (segment != null)
+                segmentGroup.LedSegments.Remove(segment);
+
+            var newGroup = new LedSegmentGroup("New Segment", segment == null ? [] : [segment], null, new(255, 255, 255));
+            dataStore.Data.Groups.Add(newGroup);
+            dataStore.Save();
+
+            return newGroup;
+        });
+
+        app.MapPut("/state/moveSegment", (
+            [FromServices] DataStoreService dataStore,
+            [Required] string segmentId,
+            string? targetGroupId) =>
+        {
+            (var segmentGroup, var segment) = dataStore.Data.Groups
+                .Select(g => (g, g.LedSegments.FirstOrDefault(x => x.ReadonlyId == segmentId)))
+                .Where(x => x.Item2 != null)
+                .FirstOrDefault();
+            if (segmentGroup == null || segment == null)
+                return Results.NotFound("The SegmentId was not found in any groups");
+
+            var targetGroup = targetGroupId == null ? null : dataStore.Data.Groups.FirstOrDefault(x => x.Id == Guid.Parse(targetGroupId));
+            if (targetGroup == null)
             {
-                dataStore.LoadFrom(JsonSerializer.Serialize(newState));
-                dataStore.Save();
+                targetGroup = new LedSegmentGroup("New Group", [], null, new(255, 255, 255));
+                dataStore.Data.Groups.Add(targetGroup);
             }
-            catch
-            {
-                // TODO: Throw exception to user?
-            }
+
+            segmentGroup.LedSegments.Remove(segment);
+            targetGroup.LedSegments.Add(segment);
+            if (segmentGroup.LedSegments.Count == 0)
+                dataStore.Data.Groups.Remove(segmentGroup);
+
+            dataStore.Save();
+
+            return Results.Accepted();
         });
     }
 }
