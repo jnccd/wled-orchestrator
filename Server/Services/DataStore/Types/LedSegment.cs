8ac42864f10658be6015cdf90ddd8b5ee87d8411
jnccd <kobidogao@outlook.com>
2024-12-29 21:02:56 +0100

    Change ledSegment to a class for var mutation

--- Server/Services/DataStore/Types/LedSegment.cs
+++ Server/Services/DataStore/Types/LedSegment.cs
@@ -1,6 +1,16 @@
 namespace Server.Services.DataStore.Types;
 
-public record LedSegment(string WledServerAddress, int SegmentIndex, string? Name = null)
+public class LedSegment(string WledServerAddress, int SegmentIndex, string? Name = null)
 {
+    public string WledServerAddress { get; } = WledServerAddress;
+    public int SegmentIndex { get; } = SegmentIndex;
+    public string? Name { get; set; } = Name;
+
     public string ReadonlyId => WledServerAddress + "#" + SegmentIndex;
+
+    public override bool Equals(object? obj) =>
+        ReadonlyId == (obj as LedSegment)?.ReadonlyId;
+
+    public override int GetHashCode() =>
+        ReadonlyId.GetHashCode();
 }
