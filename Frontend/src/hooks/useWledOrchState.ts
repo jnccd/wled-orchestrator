ef57554b0b11f5dc2d44a3595d6d92da4241af86
Niklas Carstensen <kobidogao@yahoo.com>
2024-12-29 13:26:21 +0100

    Only use types/api in react query hook

--- Frontend/src/hooks/useWledOrchState.ts
+++ Frontend/src/hooks/useWledOrchState.ts
@@ -1,7 +1,9 @@
 import apiClient from "../services/api-client";
 import { components } from "../types/api";
 
-export type LedSegmentGroups = components["schemas"]["DataStoreRoot"]
+export type LedSegmentGroups = components["schemas"]["DataStoreRoot"];
+export type LedSegmentGroup = components["schemas"]["LedSegmentGroup"];
+export type LedSegment = components["schemas"]["LedSegment"];
 
 export const getWledOrchState = () => 
   apiClient
