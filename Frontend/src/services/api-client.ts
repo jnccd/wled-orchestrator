9b7d6634c0796e84981813d38f1ddd7c7b417cac
jnccd <kobidogao@outlook.com>
2024-12-31 02:13:00 +0100

    Merge service with state hook

--- Frontend/src/services/api-client.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: import.meta.env.VITE_DEV_BACKEND_ADDRESS ? import.meta.env.VITE_DEV_BACKEND_ADDRESS : window.location.href,
-    withCredentials: false,
-})
