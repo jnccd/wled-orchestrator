ce2275351c2ec115ccf7fd7644c1a9a7125e21fa
jnccd <kobidogao@outlook.com>
2025-01-05 23:20:55 +0100

    Expose type info using special endpoint

--- Server/Endpoints/RegisterTypeInfoEndpoints.cs
+++ Server/Endpoints/RegisterTypeInfoEndpoints.cs
@@ -6,25 +6,34 @@ using Server.Services.LedTheme;
 
 namespace Server.Endpoints;
 
+record LedThemeTypes(IEnumerable<LedThemeImpl> themes, IEnumerable<LedThemeModifierImpl> modifiers);
+record LedThemeImpl(string Name, IEnumerable<LedThemeImplProperty> Properties);
+record LedThemeImplProperty(string Name, string Type);
+record LedThemeModifierImpl(string Name, IEnumerable<LedThemeModifierImplProperty> Properties);
+record LedThemeModifierImplProperty(string Name, string Type);
+
 public static class RegisterTypeInfoEndpoints
 {
     public static void RegisterLedThemeTypeInfoEndpoints(this WebApplication app)
     {
-        {
-            Type ledThemeType = typeof(LedTheme);
-            var attrs = ledThemeType.GetCustomAttributes<JsonDerivedTypeAttribute>();
-            foreach (var attr in attrs)
-            {
-                //attr.DerivedType.;
-            }
+        Type ledThemeType = typeof(LedTheme);
+        var ledThemeTypeAttrs = ledThemeType.GetCustomAttributes<JsonDerivedTypeAttribute>();
+        var themeTypesInfo = ledThemeTypeAttrs
+            .Select(x => new LedThemeImpl(
+                x.TypeDiscriminator as string ?? "Not Found",
+                x.DerivedType.GetProperties()
+                    .Where(y => y.DeclaringType == x.DerivedType)
+                    .Select(y => new LedThemeImplProperty(y.Name, y.PropertyType.Name))));
 
-            app.MapGet("/themes", (
-                [FromServices] DataStoreService dataStore) =>
-            {
-                // TODO: Get from refelction?
+        Type ledThemeModifierType = typeof(LedThemeModifier);
+        var ledThemeModifierTypeAttrs = ledThemeModifierType.GetCustomAttributes<JsonDerivedTypeAttribute>();
+        var themeModifierTypesInfo = ledThemeModifierTypeAttrs
+            .Select(x => new LedThemeModifierImpl(
+                x.TypeDiscriminator as string ?? "Not Found",
+                x.DerivedType.GetProperties()
+                    .Where(y => y.DeclaringType == x.DerivedType)
+                    .Select(y => new LedThemeModifierImplProperty(y.Name, y.PropertyType.Name))));
 
-                return Results.Accepted(value: attrs);
-            });
-        }
+        app.MapGet("/themes", () => new LedThemeTypes(themeTypesInfo, themeModifierTypesInfo));
     }
 }
--- Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
+++ Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
@@ -5,6 +5,7 @@ namespace Server.Services.LedTheme.Themes;
 public class LedThemeSingleColor(Color? color = null) : LedTheme
 {
     public Color Color { get; set; } = color ?? new(0, 255, 255);
+    public byte Brightness { get; set; } = 240;
 
-    public override LedGroupState? GetNewState(LedThemeInput input) => new([Color], 200);
+    public override LedGroupState? GetNewState(LedThemeInput input) => new([Color], Brightness);
 }
