c37a69b9a8b4bb81af1046b85fe48206e107044f
Niklas Carstensen <kobidogao@yahoo.com>
2025-01-13 17:56:10 +0100

    Enable theme property editing fro double and color

--- Frontend/src/components/WledOrchThemeEditor/WledOrchThemeEditor.tsx
+++ Frontend/src/components/WledOrchThemeEditor/WledOrchThemeEditor.tsx
@@ -14,9 +14,30 @@ import {
   MenuItem,
   MenuList,
   Box,
+  Center,
+  VStack,
+  Slider,
+  SliderMark,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import useSelectedGroupStore from "../../hooks/useLocalStore";
+import { Colorful, ColorResult, rgbaToHsva } from "@uiw/react-color";
+
+const readProperty = (obj: any, prop: string): any => {
+  return obj[prop];
+};
+
+const writeProperty = (obj: any, prop: string, newValue: any): void => {
+  obj[prop] = newValue;
+};
+
+const firstCharToLowerCase = (
+  text: string | null | undefined
+): string | undefined =>
+  !text || text === "" ? undefined : text[0]?.toLowerCase() + text.slice(1);
 
 const WledOrchThemeEditor = () => {
   const selectedGroupStore = useSelectedGroupStore();
@@ -58,7 +79,7 @@ const WledOrchThemeEditor = () => {
                   changeThemeMutation.mutate({
                     groupId: selectedGroup?.id ?? "",
                     newTheme: {
-                      $type: x.name,
+                      $type: x.typeDiscriminator,
                     },
                   })
                 }
@@ -73,14 +94,101 @@ const WledOrchThemeEditor = () => {
         {!selectedGroup?.theme ? (
           <Text>-</Text>
         ) : (
-          themeTypesQuery.data?.themes
-            ?.filter((x) => x.name === selectedGroup?.theme?.typeName)[0]
-            .properties?.map((x) => (
-              <HStack justifyContent={"center"}>
-                <Text>{x.name}:</Text>
-                <Text>{x.type} input field</Text>
-              </HStack>
-            ))
+          <Center>
+            <VStack>
+              {themeTypesQuery.data?.themes
+                ?.filter(
+                  (themeTypes) =>
+                    themeTypes.name === selectedGroup?.theme?.typeName
+                )[0]
+                .properties?.map((themeTypeProperty) => {
+                  const propertyName = firstCharToLowerCase(
+                    themeTypeProperty.name
+                  );
+                  if (!propertyName) {
+                    return <Text>Invalid property name!</Text>;
+                  }
+                  const propertyValue = readProperty(
+                    selectedGroup?.theme,
+                    propertyName
+                  );
+                  if (themeTypeProperty.type === "Color") {
+                    return (
+                      <HStack>
+                        <Text>{themeTypeProperty.name}:</Text>
+                        <Colorful
+                          color={rgbaToHsva({
+                            r: propertyValue.r,
+                            g: propertyValue.g,
+                            b: propertyValue.b,
+                            a: 1,
+                          })}
+                          onChange={(colorRes: ColorResult) => {
+                            writeProperty(
+                              selectedGroup?.theme,
+                              propertyName,
+                              colorRes.rgb
+                            );
+                            changeThemeMutation.mutate({
+                              groupId: selectedGroup.id ?? "",
+                              newTheme: selectedGroup?.theme,
+                            });
+                          }}
+                          disableAlpha
+                        ></Colorful>
+                      </HStack>
+                    );
+                  } else if (themeTypeProperty.type === "Double") {
+                    return (
+                      <HStack width={"100%"}>
+                        <Text>{themeTypeProperty.name}:</Text>
+                        <Box p={4} pt={6} width={"100%"}>
+                          <Slider
+                            aria-label="slider-ex-6"
+                            onChange={(val) => {
+                              writeProperty(
+                                selectedGroup?.theme,
+                                propertyName,
+                                val
+                              );
+                              changeThemeMutation.mutate({
+                                groupId: selectedGroup.id ?? "",
+                                newTheme: selectedGroup?.theme,
+                              });
+                            }}
+                            defaultValue={propertyValue}
+                            min={0}
+                            max={300}
+                          >
+                            <SliderMark
+                              value={propertyValue}
+                              textAlign="center"
+                              bg="blue.500"
+                              color="white"
+                              mt="-10"
+                              ml="-5"
+                              w="12"
+                              borderRadius={"6px"}
+                            >
+                              {propertyValue}
+                            </SliderMark>
+                            <SliderTrack>
+                              <SliderFilledTrack />
+                            </SliderTrack>
+                            <SliderThumb />
+                          </Slider>
+                        </Box>
+                      </HStack>
+                    );
+                  } else {
+                    <HStack justifyContent={"center"}>
+                      <Text>{themeTypeProperty.name}:</Text>
+                      <Text>{themeTypeProperty.type} input field</Text>
+                    </HStack>;
+                  }
+                })}
+            </VStack>
+          </Center>
         )}
       </Box>
     </>
