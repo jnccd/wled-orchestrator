328416fa0405d97f1cc59f221d2c41025e274286
jnccd <kobidogao@outlook.com>
2025-01-13 18:58:35 +0100

    Use full brightness

--- Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
+++ Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
@@ -5,7 +5,7 @@ namespace Server.Services.LedTheme.Themes;
 public class LedThemeSingleColor(Color? color = null) : LedTheme
 {
     public Color Color { get; set; } = color ?? new(0, 255, 255);
-    public byte Brightness { get; set; } = 240;
+    public byte Brightness { get; set; } = 255;
 
     public override LedGroupState? GetNewState(LedThemeInput input) => new([Color], Brightness);
 }
