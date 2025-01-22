da4140fe296d7ebc0bfd638838cfa63cc1162f4e
Niklas Carstensen <kobidogao@yahoo.com>
2025-01-22 19:15:19 +0100

    Show image in frontend

--- Frontend/src/components/WledOrchThemeEditor/WledOrchThemeEditor.tsx
+++ Frontend/src/components/WledOrchThemeEditor/WledOrchThemeEditor.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import {
+  baseUrl,
   getWledOrchState,
   wledOrchStateQueryKey,
 } from "../../hooks/useWledOrchApi";
-import { Text, Box, SimpleGrid, Heading } from "@chakra-ui/react";
+import { Box, SimpleGrid, Heading } from "@chakra-ui/react";
 import { useSelectedGroupStore } from "../../hooks/useLocalStore";
 import ThemePicker from "./ThemePicker";
 import ThemePropertiesEditor from "./ThemePropertiesEditor";
 import { usePageWidth } from "../../hooks/usePageWidth";
+import { Image } from "@chakra-ui/react";
 
 const WledOrchThemeEditor = () => {
   const pageWidth = usePageWidth();
@@ -26,20 +28,27 @@ const WledOrchThemeEditor = () => {
   return (
     <Box paddingTop={2}>
       <ThemePicker></ThemePicker>
-      <SimpleGrid columns={pageWidth > 780 ? 2 : 1} gap={8} padding={6}>
-        <Box display="flex" flexDirection="column">
-          {!selectedGroup?.theme ? (
-            <Text>-</Text>
-          ) : (
+      {!selectedGroup?.theme ? (
+        <></>
+      ) : (
+        <SimpleGrid columns={pageWidth > 780 ? 2 : 1} gap={8} padding={6}>
+          <Box display="flex" flexDirection="column">
             <ThemePropertiesEditor></ThemePropertiesEditor>
-          )}
-        </Box>
-        <Box border={"red solid 3px"} width={"300px"}>
-          <Heading fontSize={30} padding={4}>
-            Preview:
-          </Heading>
-        </Box>
-      </SimpleGrid>
+          </Box>
+          <Box border={"red solid 3px"} width={"300px"}>
+            <Heading fontSize={30} padding={4}>
+              Preview:
+            </Heading>
+            <Image
+              src={
+                baseUrl +
+                "/state/group/theme-preview?groupId=" +
+                selectedGroup?.id
+              }
+            ></Image>
+          </Box>
+        </SimpleGrid>
+      )}
     </Box>
   );
 };
