0b8e0b18b83f4510ed7d8add4b31f03485bef4b2
Niklas Carstensen <kobidogao@yahoo.com>
2025-01-22 19:15:08 +0100

    Add theme preview image rendering

--- Server/Endpoints/Controllers/GroupController.cs
+++ Server/Endpoints/Controllers/GroupController.cs
@@ -2,6 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Server.Services.DataStore;
 using Server.Services.LedTheme;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace Server.Endpoints.Controllers;
 
@@ -44,7 +46,7 @@ public class GroupController : ControllerBase
         {
             var group = dataStore.Data.Groups.FirstOrDefault(x => x.Id == Guid.Parse(groupId));
             if (group == null)
-                return Results.NotFound("The GroupId was not found in any groups");
+                return Results.NotFound("The GroupId was not found in any groups!");
 
             group.Name = newName;
 
@@ -65,7 +67,7 @@ public class GroupController : ControllerBase
         {
             var group = dataStore.Data.Groups.FirstOrDefault(x => x.Id == Guid.Parse(groupId));
             if (group == null)
-                return Results.NotFound("The GroupId was not found in any groups");
+                return Results.NotFound("The GroupId was not found in any groups!");
 
             group.Theme = newTheme;
 
@@ -73,5 +75,56 @@ public class GroupController : ControllerBase
         }
 
         return Results.Accepted();
+    }
+
+    [HttpGet("theme-preview")]
+    [ProducesResponseType(typeof(IResult), 200, "image/png")]
+    public IResult GetThemePreview(
+        [FromServices] DataStoreService dataStore,
+        [Required] string groupId)
+    {
+        Image<Rgba32> image;
+
+        lock (dataStore.lockject)
+        {
+            var group = dataStore.Data.Groups.FirstOrDefault(x => x.Id == Guid.Parse(groupId));
+            if (group == null)
+                return Results.NotFound("The GroupId was not found in any groups!");
+
+            if (group.Theme == null)
+                return Results.NotFound("The group does not have a theme!");
+
+            var testState = group.Theme.GetNewState(new(new(2000, 1, 1, 0, 0, 0)));
+
+            if (testState == null)
+                return Results.NotFound("Cant get state from theme!");
+
+            image = new(testState.Colors.Length, testState.Colors.Length * 16 / 9);
+            image.ProcessPixelRows(accessor =>
+            {
+                for (int y = 0; y < accessor.Height; y++)
+                {
+                    Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+
+                    var hour = 24 / (float)accessor.Height * y;
+                    var state = group.Theme.GetNewState(new(new(2000, 1, 1, (int)hour, (int)(hour % 1 * 60), 0)));
+
+                    for (int x = 0; x < pixelRow.Length; x++)
+                    {
+                        ref Rgba32 pixel = ref pixelRow[x];
+                        pixel.R = state!.Colors[x].R;
+                        pixel.G = state!.Colors[x].G;
+                        pixel.B = state!.Colors[x].B;
+                        pixel.A = 255;
+                    }
+                }
+            });
+        }
+
+        Stream imageStream = new MemoryStream(image.Width * image.Height * image.PixelType.BitsPerPixel / 8);
+        image.SaveAsPng(imageStream);
+        imageStream.Position = 0;
+
+        return Results.File(imageStream, "image/png", "theme-preview");
     }
 }
