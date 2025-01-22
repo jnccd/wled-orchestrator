b36ebc666f2ae0a8e7066e3d35d8f92a23168c62
Niklas Carstensen <kobidogao@yahoo.com>
2025-01-22 21:32:38 +0100

    Refactor

--- Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
+++ Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
@@ -5,7 +5,6 @@ namespace Server.Services.LedTheme.Themes;
 public class LedThemeSingleColor(Color? color = null) : LedTheme
 {
     public Color Color { get; set; } = color ?? new(0, 255, 255);
-    public byte Brightness { get; set; } = 255;
 
-    public override LedGroupState? GetNewState(LedThemeInput input) => new([Color], Brightness);
+    public override LedGroupState? GetNewState(LedThemeInput input) => new([Color], 255);
 }
