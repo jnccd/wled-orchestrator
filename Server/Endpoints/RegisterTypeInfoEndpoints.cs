75060c5a0e6ad0519ef1121eb320c2f0745995b3
Niklas Carstensen <kobidogao@yahoo.com>
2025-02-05 23:34:32 +0100

    Use attr for frontend form gen

--- Server/Endpoints/RegisterTypeInfoEndpoints.cs
+++ Server/Endpoints/RegisterTypeInfoEndpoints.cs
@@ -1,15 +1,25 @@
 using System.ComponentModel;
 using System.Reflection;
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Mvc;
-using Server.Services.DataStore;
 using Server.Services.LedTheme;
 
 namespace Server.Endpoints;
 
-record LedThemeTypes(IEnumerable<TypeInfo> themes, IEnumerable<TypeInfo> modifiers);
+record LedThemeTypes(IEnumerable<TypeInfo> Themes, IEnumerable<TypeInfo> Modifiers);
 record TypeInfo(string Name, string? TypeDiscriminator, IEnumerable<TypePropertyInfo> Properties);
-record TypePropertyInfo(string Name, string Type, object? defaultValue = null);
+record TypePropertyInfo(string Name, string Type, GenerateFrontendFormData Settings);
+class GenerateFrontendFormData(GenerateFrontendFormAttribute settings)
+{
+    public readonly double MinValue = settings.MinValue;
+    public readonly double MaxValue = settings.MaxValue;
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class GenerateFrontendFormAttribute(double MinValue = 0, double MaxValue = 150) : Attribute
+{
+    public readonly double MinValue = MinValue;
+    public readonly double MaxValue = MaxValue;
+}
 
 public static class RegisterTypeInfoEndpoints
 {
@@ -18,22 +28,24 @@ public static class RegisterTypeInfoEndpoints
         Type ledThemeType = typeof(LedTheme);
         var ledThemeTypeAttrs = ledThemeType.GetCustomAttributes<JsonDerivedTypeAttribute>();
         var themeTypesInfo = ledThemeTypeAttrs
-            .Select(x => new TypeInfo(
-                x.DerivedType.Name.Replace("LedTheme", ""),
-                x.TypeDiscriminator as string,
-                x.DerivedType.GetProperties()
-                    .Where(y => y.DeclaringType == x.DerivedType)
-                    .Select(y => new TypePropertyInfo(y.Name, y.PropertyType.Name, y.GetCustomAttribute<DefaultValueAttribute>()?.Value))));
+            .Select(typeAttr => new TypeInfo(
+                typeAttr.DerivedType.Name.Replace("LedTheme", ""),
+                typeAttr.TypeDiscriminator as string,
+                typeAttr.DerivedType.GetProperties()
+                    .Where(prop => prop.DeclaringType == typeAttr.DerivedType // Not from parent classes
+                        && prop.GetCustomAttribute<GenerateFrontendFormAttribute>() != null)
+                    .Select(prop => new TypePropertyInfo(prop.Name, prop.PropertyType.Name, new GenerateFrontendFormData(prop.GetCustomAttribute<GenerateFrontendFormAttribute>() ?? new GenerateFrontendFormAttribute())))));
 
         Type ledThemeModifierType = typeof(LedThemeModifier);
         var ledThemeModifierTypeAttrs = ledThemeModifierType.GetCustomAttributes<JsonDerivedTypeAttribute>();
         var themeModifierTypesInfo = ledThemeModifierTypeAttrs
-            .Select(x => new TypeInfo(
-                x.DerivedType.Name,
-                x.TypeDiscriminator as string,
-                x.DerivedType.GetProperties()
-                    .Where(y => y.DeclaringType == x.DerivedType)
-                    .Select(y => new TypePropertyInfo(y.Name, y.PropertyType.Name, y.GetCustomAttribute<DefaultValueAttribute>()?.Value))));
+            .Select(typeAttr => new TypeInfo(
+                typeAttr.DerivedType.Name,
+                typeAttr.TypeDiscriminator as string,
+                typeAttr.DerivedType.GetProperties()
+                    .Where(prop => prop.DeclaringType == typeAttr.DerivedType // Not from parent classes
+                        && prop.GetCustomAttribute<GenerateFrontendFormAttribute>() != null)
+                    .Select(prop => new TypePropertyInfo(prop.Name, prop.PropertyType.Name, new GenerateFrontendFormData(prop.GetCustomAttribute<GenerateFrontendFormAttribute>() ?? new GenerateFrontendFormAttribute())))));
 
         app.MapGet("/theme-types", () => new LedThemeTypes(themeTypesInfo, themeModifierTypesInfo));
     }
--- Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
+++ Server/Services/LedTheme/Themes/LedThemeSingleColor.cs
@@ -1,9 +1,11 @@
+using Server.Endpoints;
 using Server.Helper;
 
 namespace Server.Services.LedTheme.Themes;
 
 public class LedThemeSingleColor(ColorHsv? color = null) : LedTheme
 {
+    [GenerateFrontendForm]
     public ColorHsv Color { get; set; } = color ?? new ColorRgb(0, 255, 255).RgbToHSV();
 
     public override LedGroupState? GetNewState(LedThemeInput input) => new([Color]);
