1562ff66b5700b942586d46d558ee066737dab21
Niklas Carstensen <kobidogao@yahoo.com>
2025-02-22 23:25:38 +0100

    Update wakeup mod

--- Server/Services/LedTheme/Modifiers/WakeupModifier.cs
+++ Server/Services/LedTheme/Modifiers/WakeupModifier.cs
@@ -4,6 +4,9 @@ using Server.Services.LedTheme;
 
 class WakeupModifier : LedThemeModifier
 {
+
+    [GenerateFrontendForm]
+    public double SleepTimeMinutes { get; set; } = 400;
     [GenerateFrontendForm]
     public double FadeTimeMinutes { get; set; } = 20;
     [GenerateFrontendForm]
@@ -11,16 +14,35 @@ class WakeupModifier : LedThemeModifier
 
     const int maxBrightness = 100;
 
+    TimeSpan DoubleMinutesToTimeSpan(double min) => new(0, (int)min, (int)(min % 1 * 60));
+
+    TimeSpan DayInvariantTimeOfDayDiff(DateTime timeStamp, TimeSpan target)
+    {
+        TimeSpan[] testDiffs = [timeStamp.TimeOfDay - target, timeStamp.TimeOfDay.Add(new(24, 0, 0)) - target, timeStamp.TimeOfDay.Add(new(-24, 0, 0)) - target];
+        return testDiffs.Select(x => x.Duration()).Min();
+    }
+
     public override LedGroupState? ModifyState(LedGroupState? state, LedThemeInput input)
     {
         if (state == null)
             return null;
 
-        var minutesDiffToWakeUp = Math.Abs((input.Time.TimeOfDay - WakeUpDayTime).TotalMinutes);
-        var stateMinBrightness = (int)(maxBrightness - Math.Abs(minutesDiffToWakeUp) * maxBrightness / FadeTimeMinutes);
+        int stateMinBrightness = 0, stateMaxBrightness = 100;
+
+        var minutesDiffToWakeUp = Math.Abs(DayInvariantTimeOfDayDiff(input.Time, WakeUpDayTime).TotalMinutes);
+        stateMinBrightness = (int)(maxBrightness - Math.Abs(minutesDiffToWakeUp) * maxBrightness / FadeTimeMinutes);
+
+        var totalSleepTimeMin = SleepTimeMinutes;
+        var midSleepTime = WakeUpDayTime.Subtract(DoubleMinutesToTimeSpan(totalSleepTimeMin / 2 + FadeTimeMinutes / 4));
+        var minutesDiffToMidSleepTime = Math.Abs(DayInvariantTimeOfDayDiff(input.Time, midSleepTime).TotalMinutes);
+        var b = totalSleepTimeMin / 65;
+        stateMaxBrightness = (int)(-b * maxBrightness - Math.Abs(minutesDiffToMidSleepTime) * -b * maxBrightness / (totalSleepTimeMin / 2) + maxBrightness);
 
         foreach (var color in state.Colors)
+        {
             color.V = color.V < stateMinBrightness ? stateMinBrightness : color.V;
+            color.V = color.V > stateMaxBrightness ? stateMaxBrightness : color.V;
+        }
 
         return state;
     }
