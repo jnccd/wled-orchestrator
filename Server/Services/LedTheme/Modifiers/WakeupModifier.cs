b363236e5aa0a7e8979c46b5506fdae4aa168207
Niklas Carstensen <kobidogao@yahoo.com>
2025-02-22 06:07:09 +0100

    Properly fix WakeupModifier

--- Server/Services/LedTheme/Modifiers/WakeupModifier.cs
+++ Server/Services/LedTheme/Modifiers/WakeupModifier.cs
@@ -9,19 +9,18 @@ class WakeupModifier : LedThemeModifier
     [GenerateFrontendForm]
     public TimeSpan WakeUpDayTime { get; set; } = TimeSpan.FromHours(8);
 
+    const int maxBrightness = 100;
+
     public override LedGroupState? ModifyState(LedGroupState? state, LedThemeInput input)
     {
         if (state == null)
             return null;
 
-        var minDiffToWakeUp = (input.Time.TimeOfDay - WakeUpDayTime).TotalMinutes;
-        var minBrightness = 100 - Math.Abs(FadeTimeMinutes - minDiffToWakeUp);
-
-        if (state.Brightness < minBrightness)
-            state.Brightness = (int)minBrightness;
+        var minutesDiffToWakeUp = Math.Abs((input.Time.TimeOfDay - WakeUpDayTime).TotalMinutes);
+        var stateMinBrightness = maxBrightness - Math.Abs(minutesDiffToWakeUp) * maxBrightness / FadeTimeMinutes;
 
         foreach (var color in state.Colors)
-            color.V = color.V < minBrightness ? (int)minBrightness : color.V;
+            color.V = color.V < stateMinBrightness ? (int)stateMinBrightness : color.V;
 
         return state;
     }
