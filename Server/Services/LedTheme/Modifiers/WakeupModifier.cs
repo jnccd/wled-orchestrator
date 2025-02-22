4de37673e5bf43beecbf6823d7eed3c73d500549
Niklas Carstensen <kobidogao@yahoo.com>
2025-02-22 23:56:13 +0100

    Fix wakeup mod

--- Server/Services/LedTheme/Modifiers/WakeupModifier.cs
+++ Server/Services/LedTheme/Modifiers/WakeupModifier.cs
@@ -19,7 +19,7 @@ class WakeupModifier : LedThemeModifier
     TimeSpan DayInvariantTimeOfDayDiff(DateTime timeStamp, TimeSpan target)
     {
         TimeSpan[] testDiffs = [timeStamp.TimeOfDay - target, timeStamp.TimeOfDay.Add(new(24, 0, 0)) - target, timeStamp.TimeOfDay.Add(new(-24, 0, 0)) - target];
-        return testDiffs.Select(x => x.Duration()).Min();
+        return testDiffs.MinBy(x => x.Duration());
     }
 
     public override LedGroupState? ModifyState(LedGroupState? state, LedThemeInput input)
@@ -29,14 +29,30 @@ class WakeupModifier : LedThemeModifier
 
         int stateMinBrightness = 0, stateMaxBrightness = 100;
 
-        var minutesDiffToWakeUp = Math.Abs(DayInvariantTimeOfDayDiff(input.Time, WakeUpDayTime).TotalMinutes);
-        stateMinBrightness = (int)(maxBrightness - Math.Abs(minutesDiffToWakeUp) * maxBrightness / FadeTimeMinutes);
+        var wakeUpTimeDiff = DayInvariantTimeOfDayDiff(input.Time, WakeUpDayTime);
+        var wakeUpTimeDiffMins = wakeUpTimeDiff.TotalMinutes;
 
-        var totalSleepTimeMin = SleepTimeMinutes;
-        var midSleepTime = WakeUpDayTime.Subtract(DoubleMinutesToTimeSpan(totalSleepTimeMin / 2 + FadeTimeMinutes / 4));
-        var minutesDiffToMidSleepTime = Math.Abs(DayInvariantTimeOfDayDiff(input.Time, midSleepTime).TotalMinutes);
-        var b = totalSleepTimeMin / 65;
-        stateMaxBrightness = (int)(-b * maxBrightness - Math.Abs(minutesDiffToMidSleepTime) * -b * maxBrightness / (totalSleepTimeMin / 2) + maxBrightness);
+        if (wakeUpTimeDiffMins < -SleepTimeMinutes - FadeTimeMinutes * 2) { }
+        else if (wakeUpTimeDiffMins < -SleepTimeMinutes - FadeTimeMinutes)
+        {
+            var timeScalar = (wakeUpTimeDiffMins - (-SleepTimeMinutes - FadeTimeMinutes * 2)) / FadeTimeMinutes;
+            stateMaxBrightness = (int)(100 - 100 * timeScalar);
+        }
+        else if (wakeUpTimeDiffMins < -FadeTimeMinutes)
+        {
+            stateMaxBrightness = 0;
+        }
+        else if (wakeUpTimeDiffMins < 0)
+        {
+            var timeScalar = (wakeUpTimeDiffMins - (-FadeTimeMinutes)) / FadeTimeMinutes;
+            stateMaxBrightness = (int)(100 * timeScalar);
+            stateMinBrightness = (int)(100 * timeScalar);
+        }
+        else if (wakeUpTimeDiffMins < FadeTimeMinutes)
+        {
+            var timeScalar = (wakeUpTimeDiffMins - 0) / FadeTimeMinutes;
+            stateMinBrightness = (int)(100 - 100 * timeScalar);
+        }
 
         foreach (var color in state.Colors)
         {
