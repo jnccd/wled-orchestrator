6783da38cc0cefd41417601c31aba24e8853eadf
jnccd <kobidogao@outlook.com>
2025-08-15 11:36:29 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,16 +5,12 @@
   description = "Nix Shell Wrapper";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system: let
-          pkgs = import nixpkgs {inherit system;};
-        in {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in { devShells.default = import ./shell.nix { inherit pkgs; }; });
 }
