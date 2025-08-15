0d8be635983adde1f9be5809b1d2ab8046548c4a
jnccd <kobidogao@outlook.com>
2025-08-15 10:29:23 +0200

    Add nixos stuff

---
+++ shell.nix
@@ -0,0 +1,5 @@
+# nix-shell --command "bash update_and_start.sh"
+# export NIXPKGS_ALLOW_INSECURE=1 && nix-shell --impure --command "bash update_and_start.sh"
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell { packages = [ icu dotnet-sdk dotnet-ef pnpm nodejs_22 ]; }
